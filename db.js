73d23d2ba3d58d2762b4463e4d2d9510e821c166
dhakarajay359-commits <dhakarajay359@gmail.com>
2026-08-12 10:57:54 +0530

    chore: add render.yaml blueprint and update db.js for custom persistent disk

--- db.js
+++ db.js
@@ -2,7 +2,14 @@ const Database = require('better-sqlite3');
 const path = require('path');
 const bcrypt = require('bcryptjs');
 
-const db = new Database(path.join(__dirname, 'data', 'ctf.db'));
+const fs = require('fs');
+
+const dataDir = process.env.DATA_DIR || path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const db = new Database(path.join(dataDir, 'ctf.db'));
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
 
